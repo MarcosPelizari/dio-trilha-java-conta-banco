75935d989e9514a9825f884a49f1d67f91cdcbb3
Marcos Pelizari <m.pelizari@gmail.com>
2024-03-20 17:38:38 -0300

    Finalizado o primeiro projeto pela Dio sobre java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da Agência !");
+        int agencia = scanner.nextInt();
+
+
+        System.out.println("Por favor, digite o número da Conta !");
+        String conta = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu Nome e Sobrenome !");
+        String nomeCliente = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o valor a ser depositado !");
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem de conta criada
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %s e seu saldo %.2f já está disponível para saque",
+                nomeCliente, agencia, conta, saldo);
+
+        scanner.close();
     }
 }
